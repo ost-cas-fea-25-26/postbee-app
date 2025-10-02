e03554ad945834eb0fd2bf7e531111e5c28a3f34
Ricardo Coelho <devel@rmorgado.ch>
2025-10-02 16:55:35 +0200

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ export default function Home() {
     <div className="flex h-dvh items-center justify-center">
       <h1>PostBee</h1>
       <img src="/images/PostBee-Logo.png" alt="Logo" width="400" />
-      <Button label="Button" primary>
-        Click me
-      </Button>
+      <Button label="Button" primary={true}></Button>
     </div>
   );
 }
