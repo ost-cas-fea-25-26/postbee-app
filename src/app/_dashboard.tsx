0e5b7e3380ab6580abbfe93d6e8f364c7d76a926
Ricardo Coelho <devel@rmorgado.ch>
2025-11-26 20:28:54 +0100

    WIP

--- src/app/_dashboard.tsx
+++ src/app/_dashboard.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { AuthLoginButton, AuthLogoutButton } from '@/components/auth';
 import { PostsList } from '@/components/posts';
 import { Heading } from '@postbee/postbee-ui-lib';
 
@@ -12,9 +11,6 @@ export function Dashboard({ session }: { session: any }) {
           <p>
             You are logged in as {session.user?.name} ({session.user?.email}).
           </p>
-          <div>
-            <AuthLogoutButton />
-          </div>
 
           <Heading level={2}>Willkommen auf Mumble</Heading>
           <Heading level={4}>Voluptatem qui cumque voluptatem quia tempora dolores distinctio vel repellat dicta.</Heading>
@@ -24,9 +20,6 @@ export function Dashboard({ session }: { session: any }) {
       ) : (
         <div>
           <p>You are not logged in.</p>
-          <div>
-            <AuthLoginButton />
-          </div>
         </div>
       )}
     </div>
