f11d2206d52fee8b0a670ed5e0818b1400c03aaa
Ricardo Coelho <devel@rmorgado.ch>
2025-11-28 16:15:03 +0100

    fix: ssr issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { getSession } from '@/lib/auth';
+import { Suspense } from 'react';
+
 import { Heading } from '@postbee/postbee-ui-lib';
 
 import { Dashboard } from './_dashboard';
 
 export default async function Home() {
-  const session = await getSession();
-
   return (
     <>
       <Heading level={2} className="text-primary">
         Willkommen auf Mumble
       </Heading>
-      <Dashboard session={session} />
+      <Suspense>
+        <Dashboard />
+      </Suspense>
     </>
   );
 }
