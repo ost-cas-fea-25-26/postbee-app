aecb79f9b92e9260551a2fdd6286325e158264ca
Ricardo Coelho <devel@rmorgado.ch>
2025-11-28 18:30:49 +0100

    WIP

--- src/components/posts/PostItem.tsx
+++ src/components/posts/PostItem.tsx
@@ -1,7 +1,29 @@
 'use client';
 
+import { PostItemUserInfo } from '@/components/posts/PostItemUserInfo';
 import { Post } from '@/lib/api';
+import { decodeULIDTimestamp } from '@/lib/utils/api';
+import { CommentsButton, CopyButton, LikeButton } from '@postbee/postbee-ui-lib';
 
 export const PostItem = ({ post }: { post: Post }) => {
-  return <pre>{JSON.stringify(post, null, 2)}</pre>;
+  return (
+    <div className="grid gap-sm sm:gap-md">
+      <div className="flex">
+        <PostItemUserInfo user={post.creator} postDate={decodeULIDTimestamp(post.id)} />
+      </div>
+      {post.text}
+      <pre>{JSON.stringify(post, null, 2)}</pre>
+      {post.text && <div className="cursor-auto whitespace-pre-wrap break-all" />}
+      {post.mediaUrl && <div className="grid cursor-auto place-content-center object-contain">TODO: Media URL</div>}
+      <div className="flex">
+        <div className="-ml-xs flex flex-wrap gap-xxs gap-y-0 sm:gap-lg" onClick={(e) => e.stopPropagation()}>
+          <CommentsButton />
+
+          <LikeButton />
+
+          <CopyButton />
+        </div>
+      </div>
+    </div>
+  );
 };
