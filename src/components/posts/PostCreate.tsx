eb0b4a32262393dcb0c05a714405310a276576d3
Ricardo Coelho <devel@rmorgado.ch>
2025-12-02 21:50:47 +0100

    feat(Post): add media upload

--- src/components/posts/PostCreate.tsx
+++ src/components/posts/PostCreate.tsx
@@ -1,24 +1,60 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+
 import { createPost } from '@/actions/posts';
 import { Form } from '@/components/core/Form';
 import { PostCard } from '@/components/posts/PostCard';
-import { Button, Heading, Textarea } from '@postbee/postbee-ui-lib';
+import { Button, Dialog, Heading, Textarea, Upload } from '@postbee/postbee-ui-lib';
+import Image from 'next/image';
 import { useFormContext } from 'react-hook-form';
 
 type PostFormData = {
   postContent: string;
+  media?: File | undefined;
 };
 
 const PostFormFields = () => {
   const {
     register,
+    setValue,
     formState: { errors },
   } = useFormContext<PostFormData>();
+
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreviewUrl(null);
+      return;
+    }
+
+    const url = URL.createObjectURL(selectedFile);
+    setPreviewUrl(url);
+
+    return () => {
+      URL.revokeObjectURL(url);
+    };
+  }, [selectedFile]);
+
+  const handleDialogSubmit = () => {
+    setValue('media', selectedFile || undefined, { shouldValidate: true });
+    setOpenDialog(false);
+  };
 
   return (
     <>
       <Heading level={4}>Hey, let's mumble ?</Heading>
+
+      {previewUrl && (
+        <div className="grid cursor-auto place-content-center object-contain space-y-2">
+          <Image src={previewUrl} alt="post-media" width={320} height={584} className="rounded-md object-cover" />
+
+          <Button icon="cancel" text="Remove" onClick={() => setSelectedFile(null)} variant="secondary" />
+        </div>
+      )}
 
       <Textarea
         {...register('postContent', { required: 'Bitte gib deinen Beitrag ein.' })}
@@ -35,8 +71,21 @@ const PostFormFields = () => {
           icon="upload"
           fullWidth
           type="button"
-          onClick={(e) => e.stopPropagation()}
+          onClick={(e) => {
+            e.stopPropagation();
+            setOpenDialog(true);
+          }}
         />
+
+        <Dialog title="Upload" open={openDialog} onClose={() => setOpenDialog(false)} onSubmit={handleDialogSubmit}>
+          <Upload
+            files={selectedFile ? [{ file: selectedFile, preview: URL.createObjectURL(selectedFile) }] : []}
+            onChange={(files) => {
+              setSelectedFile(files[0]?.file ?? null);
+            }}
+          />
+        </Dialog>
+
         <Button text="Absenden" icon="send" fullWidth type="submit" onClick={(e) => e.stopPropagation()} />
       </div>
     </>
@@ -45,14 +94,14 @@ const PostFormFields = () => {
 
 export const PostCreate = () => {
   const onSubmit = (data: PostFormData) => {
-    console.log('Submitted post:', data.postContent);
+    console.log('Submitted post:', data);
     // TODO: add post to current list
-    const res = createPost(data.postContent);
+    const res = createPost(data.postContent, data.media);
   };
 
   return (
     <PostCard>
-      <Form<PostFormData> onSubmit={onSubmit} className="grid gap-sm sm:gap-md">
+      <Form<PostFormData> onSubmit={onSubmit} className="grid gap-sm sm:gap-md" defaultValues={{ media: null }}>
         <PostFormFields />
       </Form>
     </PostCard>
