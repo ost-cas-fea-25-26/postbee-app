9e6b59220ecf6b6fd9dfa7f36b28758fd936f1b4
Ricardo Coelho <devel@rmorgado.ch>
2025-12-03 08:18:32 +0100

    fix(Post): issue when access to params and Suspense usage

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -12,27 +12,34 @@ interface Props {
   }>;
 }
 
-export default async function Page({ params }: Props) {
+async function PostContent({ params }: { params: Promise<{ id: string }> }) {
   const postId = (await params).id;
   const { data: post } = await getPostsById({ path: { id: postId } });
-
   const session = await getSession();
 
+  if (!post) {
+    return <p>Post not found</p>;
+  }
+
+  return (
+    <PostCard post={post}>
+      <PostItem post={post} session={session} />
+      <Suspense>
+        <CommentCreate postId={postId} session={session} />
+      </Suspense>
+      <Suspense fallback={<p>Loading comments...</p>}>
+        <CommentsList postId={postId} />
+      </Suspense>
+    </PostCard>
+  );
+}
+
+export default function Page({ params }: Props) {
   return (
     <div>
-      {post && (
-        <PostCard post={post}>
-          <PostItem post={post} session={session} />
-
-          <Suspense>
-            <CommentCreate postId={postId} session={session} />
-          </Suspense>
-
-          <Suspense fallback={<p>Loading posts...</p>}>
-            <CommentsList postId={postId} />
-          </Suspense>
-        </PostCard>
-      )}
+      <Suspense fallback={<p>Loading post...</p>}>
+        <PostContent params={params} />
+      </Suspense>
     </div>
   );
 }
