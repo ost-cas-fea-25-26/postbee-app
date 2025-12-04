acaa3db4b940c7478e7a08c4cd4766b75252576a
Ricardo Coelho <devel@rmorgado.ch>
2025-12-04 20:53:34 +0100

    feat(auth): set custom options for cookiesCache

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -23,6 +23,18 @@ export type AuthUser = {
 };
 
 export const auth = betterAuth({
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 7 * 24 * 60 * 60, // 7 days cache duration
+      strategy: 'jwt', // can be "jwt" or "compact"
+      refreshCache: true, // Enable stateless refresh
+    },
+  },
+  account: {
+    storeStateStrategy: 'cookie',
+    storeAccountCookie: true, // Store account data after OAuth flow in a cookie (useful for database-less flows)
+  },
   user: {
     additionalFields: {
       identifier: {
