522f395c49c9c1575c6dcff5255d9c26c2ac7a74
Ricardo Coelho <devel@rmorgado.ch>
2025-12-05 09:58:32 +0100

    feat(IconLabel): add initial version

--- src/components/posts/PostItemUserInfo.tsx
+++ src/components/posts/PostItemUserInfo.tsx
@@ -1,7 +1,8 @@
 import React, { ComponentProps, ReactNode } from 'react';
 
+import { IconLabel } from '@/components/core/IconLabel';
 import { readableCreatedDate } from '@/lib/utils';
-import { Icon, Label } from '@postbee/postbee-ui-lib';
+import { Label } from '@postbee/postbee-ui-lib';
 import Link from 'next/link';
 
 interface IPostItemUserInfo {
@@ -21,27 +22,24 @@ export const PostItemUserInfo = ({ username, displayName, postDate, trailing }: 
     <div className="flex justify-between items-center w-full">
       <Link
         href={'#TODO'}
-        className="duratin-300 relative flex place-items-center gap-xs rounded-sm transition-all hover:scale-105"
+        className="duratin-300 relative flex place-items-center gap-xs rounded-sm transition-all"
         data-testid="mumble-user-info"
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex flex-col gap-xs">
           <Label {...labelProps} className="max-w-[150px] truncate capitalize sm:max-w-none" />
+
           <div className="flex flex-wrap gap-sm items-center">
             {username && (
-              <span className="flex flex-wrap gap-xs items-center text-primary">
-                <Icon icon={'profile'} size={12} />
+              <IconLabel color="primary" icon="profile">
                 {username}
-              </span>
+              </IconLabel>
             )}
 
             {postDate && (
-              <>
-                <span className="flex flex-wrap gap-xs items-center text-secondary">
-                  <Icon icon={'time'} size={12} />
-                  {readableCreatedDate(postDate)}
-                </span>
-              </>
+              <IconLabel color="secondary-400" icon="time">
+                {readableCreatedDate(postDate)}
+              </IconLabel>
             )}
           </div>
         </div>
