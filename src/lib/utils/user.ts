675ac0e6b8539158feced135b25694cae39c0f18
Ricardo Coelho <devel@rmorgado.ch>
2025-12-12 07:37:37 +0100

    feat(Avatar): add fallback user initials

--- src/lib/utils/user.ts
+++ src/lib/utils/user.ts
@@ -3,5 +3,17 @@ import { AppUser } from '@/lib/types';
 export const getUserDisplayName = (user: AppUser) => {
   const username = user?.username ?? '';
 
-  return user?.firstname && user?.lastname ? `${user.firstname} ${user.lastname}` : user?.name ? user.name : username;
+  return (
+    user?.displayName ??
+    (user?.firstname && user?.lastname ? `${user.firstname} ${user.lastname}` : user?.name ? user.name : username)
+  );
 };
+
+export function getUserInitials(name = '') {
+  return name
+    .trim()
+    .split(/\s+/)
+    .map((word) => word[0]?.toUpperCase() ?? '')
+    .slice(0, 2)
+    .join('');
+}
