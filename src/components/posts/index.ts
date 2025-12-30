ed0b2e52f247e4d432be99d41972ad3b502ad703
Ricardo Coelho <devel@rmorgado.ch>
2025-12-30 12:00:14 +0100

    fix: eslint issues

--- src/components/posts/index.ts
+++ src/components/posts/index.ts
@@ -1,8 +1,5 @@
 export { PostCard } from './PostCard';
-export { PostCreate } from './PostCreate';
 export { PostItem } from './PostItem';
-export { PostItemEditDialog } from './PostItemEditDialog';
-export { PostItemUserInfo } from './PostItemUserInfo';
 export { PostsProvider } from './PostsProvider';
 export { PostsList } from './PostsList';
 export { PostsListClient } from './PostsListClient';
