7d7cddf8206299b3e5a552e10945a29995b6fb38
Vittorio Costantini <vittorio.costantini@abacus.ch>
2026-01-01 21:57:07 +0100

    Revert "fix(auth): try to fix auth on production"
    This reverts commit 84a239119a55f07520f31703ef0ed026610dac2f.

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -54,7 +54,7 @@ export const auth = betterAuth({
           mapProfileToUser: (profile: OAuthProfile) => {
             return {
               identifier: profile.sub,
-              email: profile.email,
+              username: profile.preferred_username,
             };
           },
         },
