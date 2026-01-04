287a054fcf03eafd000d2dd56cc8e1c80415c4c7
Vittorio Costantini <vittorio.costantini@abacus.ch>
2026-01-04 08:37:35 +0100

    fix(tags/a11y): make links more recognisable

--- src/lib/utils/tags.ts
+++ src/lib/utils/tags.ts
@@ -6,6 +6,6 @@ export const textToTagsLink = (text: string) => {
   return text.replace(/(^|[^/\w])#([\p{L}\p{N}_-]+)/gu, (full, prefix: string, tag: string) => {
     const encoded = encodeURIComponent(tag);
 
-    return `${prefix}<a href="/?tags=${encoded}" class="text-primary-600 hover:text-primary-900 hover:underline">#${tag}</a>`;
+    return `${prefix}<a href="/?tags=${encoded}" class="text-primary-700 underline decoration-dotted decoration-primary-400 decoration-1 underline-offset-3 hover:decoration-primary-700 hover:decoration-solid pr-xxs">#${tag}</a>`;
   });
 };
