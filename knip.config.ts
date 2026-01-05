a33b0375a39797d8bd2eb46a79e68f15cc231a51
Vittorio Costantini <vittorio.costantini@abacus.ch>
2026-01-05 21:42:59 +0100

    fix(knip): ignore pa11yci config file

--- knip.config.ts
+++ knip.config.ts
@@ -4,6 +4,7 @@ const config: KnipConfig = {
   ignore: [
     'src/lib/api/**', // Ignore all auto-generated API client files
     'openapi-ts.config.ts', // Ignore OpenAPI config file
+    '.pa11yci.js',
   ],
   ignoreDependencies: [
     'dotenv', // Used in openapi-ts.config.ts
